e175f4a5acaf89e0badcce7d14c8210b6848ef03
CesarQuintero1 <7205892-CesarQuintero1@users.noreply.replit.com>
2023-03-20 19:52:54 +0000

    Initial commit

---
+++ 08_condicional.py
@@ -0,0 +1,12 @@
+# Condicional if
+adivinar = 42
+numero = int(input('Sr usuario, digite un número: '))
+if (numero > adivinar):
+  print('Bájele el volumen')
+elif (numero < adivinar): 
+  print('Subale el volumen')
+else:
+  print("VERDADERO")
+
+
+print('La instrución "IF" terminó. ')
